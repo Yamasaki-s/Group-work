cdc4a07a72fd4d7f5dd520baeebdb7dc683ee110
Yamasaki.s <110821902+Yamasaki-s@users.noreply.github.com>
2023-01-03 16:10:09 +0900

    Create Constant.java

---
+++ Constant.java
@@ -0,0 +1,14 @@
+package jp.co.masquerade.constant;
+
+//定数や定数クラスを自動生成する
+public class Constant {
+	public static final String SESSION_KEY_LOGIN_USER = "SESSION_KEY_LOGIN_USER";
+
+	public static String URL = "jdbc:postgresql://localhost:5432/masq";
+	public static String DB_USER = "postgres";
+	public static String DB_PASSWORD = "root";
+	
+	public static String EVENT_TYPE_TALK = "N001";
+	public static String EVENT_TYPE_LIKE = "N002";
+	
+}
