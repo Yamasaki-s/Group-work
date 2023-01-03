383b9dbe76932b6c12d4833fbfeae9d5931f4ba8
Yamasaki.s <110821902+Yamasaki-s@users.noreply.github.com>
2023-01-03 16:21:38 +0900

    Create AbstractServlet.java

---
+++ AbstractServlet.java
@@ -0,0 +1,11 @@
+package jp.co.masquerade.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * すべてのサーブレットの親クラス
+ */
+public abstract class AbstractServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
